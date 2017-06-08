bca21e25f4ba8e83094511c18779e28b871700f2
Aaron Figueroa <nothing>
2017-06-08 18:21:10 -0400

    Adding default props to the mix

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Todo from './Todo';
 class App extends React.Component {
 
 // Establish a function that we are going to be passing
-// into the Todo Component
+// into the Todo Component. 
   fun(e){
     alert("We're about to get White Castle");
   }
@@ -20,7 +20,7 @@ class App extends React.Component {
       <div>
         <Todo name="Pick up My cousins kids"
               age="24" data={data}
-              tiger={this.fun} />
+              alert={this.fun} />
       </div>
     );
   }
@@ -35,5 +35,37 @@ const data = {'todo': [1,2,3,4]}
 // passing a number
 // const data = 4
 
+// inserting objects within an array
+// App.defaultProps=[{
+//   name: "whatever",
+//   age: 24,
+//   single: true
+//
+// }]
+
+// Just an object with properties
+// App.defaultProps={
+//   name: "whatever",
+//   age: 24,
+//   single: true
+//
+// }
+
+// Passing an object with one key and two elements in an array
+App.defaultProps={
+
+  "status": [{
+  name: "whatever",
+  age: 24,
+  single: true
+  },
+  {
+    name: "whomever",
+    age: 99,
+    single: false
+  }]
+
+
+}
 
 export default App;
