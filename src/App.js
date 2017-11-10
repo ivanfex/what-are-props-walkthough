2379a36283492b3283bce11ea4b8bc843fa82568
Ivan Eduardo Felix Herrera <ivanfex@Ivans-MacBook-Pro.local>
2017-11-09 19:59:23 -0500

    Made changes to App and Todo

--- src/App.js
+++ src/App.js
@@ -11,6 +11,13 @@ class App extends React.Component {
   constructor(){
     super();
 
+
+
+    this.state={
+        tasks: ['first', 'second', 'third'],
+        input: 'hola',
+        completed: false
+    }
   }
 
 // Establish a function that we are going to be passing
@@ -18,23 +25,40 @@ class App extends React.Component {
 
   handleSubmit(e){
     e.preventDefault();
+    this.setState({
+        tasks: this.state.tasks.concat([this.state.input]),
+        input: ''
+    })
   }
 
   handleChange(e){
     e.preventDefault();
-  
+    this.setState({
+        input: e.target.value
+    })
   }
 
   render() {
+      console.log(this.state);
+      let tasks = this.state.tasks.map((task, index)=>
+        <div>
+          <li key={index}>{task}</li>
+          <button onClick={}>
+              Complete
+          </button>
+      </div>
+      )
+
     return (
       <div>
 
         <form onSubmit={(event)=> this.handleSubmit(event)}>
-          <input onChange={()=>this.handleChange(event)} />
+          <input onChange={(event)=>this.handleChange(event)}/>
           <input type="submit"/>
         </form>
-
+        <Todo task={tasks}/>
       </div>
+
     );
   }
 }
--- src/Todo.js
+++ src/Todo.js
@@ -7,7 +7,7 @@ class Todo extends React.Component{
 render(){
   return(
     <div>
-    
+        {this.props.task}
     </div>
   )
 }
